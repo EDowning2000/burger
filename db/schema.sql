969dbdf0e57d3bf08ac166f330b664403336678f
EDowning2000 <emersondowning@gmail.com>
2020-04-01 12:51:37 -0400

    seeds file done

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ USE burgers_db;
 CREATE TABLE burgers (
 	id INT NOT NULL AUTO_INCREMENT,
 	burger_name INT(40) NOT NULL,
-	devoured INT(10) NOT NULL, 
+	devoured BOOLEAN DEFAULT false, 
   PRIMARY KEY (id)
 );
