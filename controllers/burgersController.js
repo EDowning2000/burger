71847f2d7f94ee73bbdae80bde38a3fee7a3c864
EDowning2000 <emersondowning@gmail.com>
2020-04-03 19:34:14 -0400

    get and post routes are working in the controller file

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,61 +1,25 @@
 const express=require('express');
-const modelBurger= require('../models/burger.js')
-const router = express.Router()
-
-var express = require("express");
-var router = express.Router();
-var express = require("express");
-var router = express.Router();
+const burger= require('../models/burger.js');
+const router = express.Router();
 
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/cat.js");
-// Create all our routes and set up logic within those routes where required.
+
+
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.selectAll(function(data) {
     var hbsObject = {
-      cats: data
+      burger: data
     };
     console.log(hbsObject);
-
     //instead of json, this is how it renders to handlebars
     res.render("index", hbsObject);
   });
 });
-router.post("/api/cats", function(req, res) {
-  cat.create([
-    "name", "sleepy"
-  ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
-});
-router.put("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-  console.log("condition", condition);
-  cat.update({
-    sleepy: req.body.sleepy
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
+
+router.post('/api/burgers', (req,res)=>{
+  burger.insertOne(["burger_name", "devoured"],[req.body.burger_name, req.body.devoured],(result)=>{
+    res.json({ id: result.insertId})//sends back the id of the new burger
     }
-  });
-});
-router.delete("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-  cat.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-// Export routes for server.js to use.
-module.exports = router;
+  )
+})
+router.put
