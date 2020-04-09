44a7d5cf96fa3c80807e836b75109be6c1a5198d
EDowning2000 <emersondowning@gmail.com>
2020-04-08 22:06:34 -0400

    random piece removed

--- config/connection.js
+++ config/connection.js
@@ -11,15 +11,7 @@ if(process.env.JAWSDB_URL){
     database: 'burgers_db'
   });
   
-};
-
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'burgers_db'
-});
+}
 
 connection.connect(function(err){
   if (err){
